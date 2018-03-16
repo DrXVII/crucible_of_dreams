7da8cd2a595d0673654995dff5566c917162dbef
DrXVII <egined@gmail.com>
2018-03-16 15:28:28 +0200

    docs update

---
+++ DEV/project.mm
@@ -0,0 +1,126 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="crucible_of_dreams" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1521199921940"><hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="Ubuntu" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<font NAME="Ubuntu"/>
+<node TEXT="GUI" POSITION="right" ID="ID_645972176" CREATED="1521199856441" MODIFIED="1521199866685">
+<edge COLOR="#ff0000"/>
+<node TEXT="main view" ID="ID_1378555589" CREATED="1521201067961" MODIFIED="1521201078954">
+<node TEXT="game_text_log" ID="ID_1148108455" CREATED="1521199882916" MODIFIED="1521200958291"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The log of what's happening around the player.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      e.g.
+    </p>
+    <p>
+      &quot;character hits eldrich being for 4 points of damage, eldrich being is bleeding, eldrich being is weakened&quot;
+    </p>
+    <p>
+      &quot;the party goes down the dark staircase for what seems an eternity&quot;
+    </p>
+    <p>
+      &quot;the character swings at the thug, the thug dodges&quot;
+    </p>
+    <p>
+      &quot;the party emerges into bright daylight&quot;
+    </p>
+    <p>
+      etc...
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="scrolling" ID="ID_1112075760" CREATED="1521201029361" MODIFIED="1521201037635"/>
+<node TEXT="object and context dependant colouring" ID="ID_784830188" CREATED="1521200982010" MODIFIED="1521201005595"/>
+<node TEXT="resizing" ID="ID_836975339" CREATED="1521200961482" MODIFIED="1521200966396"/>
+</node>
+<node TEXT="functions (sidebar?)" ID="ID_87503033" CREATED="1521201261809" MODIFIED="1521201316938">
+<node TEXT="main menu" ID="ID_1819114896" CREATED="1521201320872" MODIFIED="1521201330408"/>
+<node TEXT="inventory" ID="ID_1549839412" CREATED="1521201330912" MODIFIED="1521201341352"/>
+<node TEXT="journal" ID="ID_1564264248" CREATED="1521201341912" MODIFIED="1521201344041"/>
+<node TEXT="abilities" ID="ID_685870306" CREATED="1521201344595" MODIFIED="1521201351353"/>
+<node TEXT="spells" ID="ID_1778675867" CREATED="1521201351840" MODIFIED="1521201353183"/>
+<node TEXT="rest" ID="ID_1242826233" CREATED="1521201353840" MODIFIED="1521201356760"/>
+<node TEXT="time of day (skyline)" ID="ID_97537996" CREATED="1521201573360" MODIFIED="1521201594888"/>
+<node TEXT="pause" ID="ID_1929336685" CREATED="1521201596752" MODIFIED="1521201600719"/>
+</node>
+<node TEXT="character roster" ID="ID_339209899" CREATED="1521201125529" MODIFIED="1521201139288">
+<node TEXT="hp" ID="ID_860060539" CREATED="1521201147295" MODIFIED="1521201155512"/>
+<node TEXT="name" ID="ID_220558468" CREATED="1521201155912" MODIFIED="1521201157056"/>
+<node TEXT="select character on_click" ID="ID_570700481" CREATED="1521201166121" MODIFIED="1521201203408"/>
+<node TEXT="status icon(s)" ID="ID_680313737" CREATED="1521206766626" MODIFIED="1521206780457"/>
+<node TEXT="? portait" ID="ID_643856696" CREATED="1521201159145" MODIFIED="1521201162554"/>
+</node>
+<node TEXT="direction of north" ID="ID_563071002" CREATED="1521201090113" MODIFIED="1521201123420"/>
+<node TEXT="? date year/month/day" ID="ID_1172604771" CREATED="1521206724946" MODIFIED="1521206749579"/>
+</node>
+</node>
+<node TEXT="gameplay" POSITION="right" ID="ID_1493560594" CREATED="1521199867552" MODIFIED="1521199878866">
+<edge COLOR="#0000ff"/>
+<node TEXT="time of day" ID="ID_876211035" CREATED="1521206831757" MODIFIED="1521206835050"/>
+<node TEXT="combat" ID="ID_1448152466" CREATED="1521206835603" MODIFIED="1521206842546">
+<node TEXT="turn based or real-time with pause ?" ID="ID_498224140" CREATED="1521206851402" MODIFIED="1521206877858"/>
+</node>
+</node>
+</node>
+</map>
