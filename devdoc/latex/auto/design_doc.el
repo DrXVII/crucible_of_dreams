1de2497814a5dec7d737f5962eca02d6b6a5c743
DrXVII <egined@gmail.com>
2019-04-06 17:56:32 +0300

    design brainstorm

--- devdoc/latex/auto/design_doc.el
+++ devdoc/latex/auto/design_doc.el
@@ -6,12 +6,12 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("xcolor" "table")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -23,14 +23,18 @@
     "hyperref"
     "xcolor")
    (TeX-add-symbols
+    '("admonition" ["argument"] 2)
     '("textbi" 1)
+    '("think" 1)
+    '("note" 1)
     '("TODO" 1)
     "Projectname"
     "Enginename"
     "Gamename")
    (LaTeX-add-xcolor-definecolors
-    "milkpink"
-    "rose"
-    "red"))
+    "red"
+    "globaltablecolor"
+    "blue"
+    "orange"))
  :latex)
 
