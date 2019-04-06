38e7e2dc007783a6caf7e273f1985a106819399c
DrXVII <egined@gmail.com>
2019-04-06 11:29:27 +0300

    admonitions for latex docs

--- devdoc/latex/auto/design_doc.el
+++ devdoc/latex/auto/design_doc.el
@@ -5,6 +5,14 @@
                      '(("book" "a4paper" "10pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("xcolor" "table")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "book"
@@ -13,12 +21,16 @@
     "mathtools"
     "minted"
     "hyperref"
-    "tabu"
     "xcolor")
    (TeX-add-symbols
+    '("textbi" 1)
     '("TODO" 1)
     "Projectname"
     "Enginename"
-    "Gamename"))
+    "Gamename")
+   (LaTeX-add-xcolor-definecolors
+    "milkpink"
+    "rose"
+    "red"))
  :latex)
 
