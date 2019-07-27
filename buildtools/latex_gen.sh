8c41815846753c967b41f18d676fe8578a3ad30d
DrXVII <egined@gmail.com>
2019-07-27 08:58:54 +0100

    updated latex build tool

--- buildtools/latex_gen.sh
+++ buildtools/latex_gen.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
 
-#latex has to be run several times (up to 3) to compile well (e.g. when there is a multi-page table of contents)
+#latex has to be run several times (up to 3) to compile well (e.g. when there
+#   is a multi-page table of contents)
 runs_needed=3
 runs_done=0
 
 filepath=$1
+cleanup_flag=$2
+filedir=$(dirname "${filepath}")
+filename=$(basename "${filepath}")
 
 if [ "$filepath" == "" ]; then
     echo "no file specified"
     exit 1
 fi
 
-filedir=$(dirname "${filepath}")
-filename=$(basename "${filepath}")
+function rm_generated_files () {
+    local filename_nosuf=$(basename "${filename}" .tex)
+    rm -fv "$filename_nosuf.aux"
+    rm -fv "$filename_nosuf.pyg"
+    rm -fv "$filename_nosuf.out"
+    rm -fv "$filename_nosuf.toc"
+    rm -fv "$filename_nosuf.log"
+}
 
-#some latex modules are capricious and want the latex compiler to be called from the same directory where the source file is
+function mv_generated_files () {
+    local filename_nosuf=$(basename "${filename}" .tex)
+    local aux_dirname="${filename_nosuf}_build_files"
+    mkdir -p "${filename_nosuf}_build_files"
+    mv -fv "$filename_nosuf.aux" \
+       "$filename_nosuf.pyg" \
+       "$filename_nosuf.out" \
+       "$filename_nosuf.toc" \
+       "$filename_nosuf.log" \
+       "$aux_dirname"
+    rm -rf "$aux_dirname/_minted-$filename_nosuf"
+    mv -fv "_minted-$filename_nosuf" \
+       "$aux_dirname/"
+}
+
+#some latex modules are capricious and want the latex compiler to be called
+#   from the same directory where the source file is
 cd $filedir
 
-#remove auxilary latex files before we start, *.aux has caused compilation problems before
-rm -f *.aux
-rm -f *.pyg
-rm -f *.out
-rm -f *.toc
+#remove auxilary latex files before we start, leftover *.aux has caused
+#   compilation problems before
+rm_generated_files
 
 while [ $runs_done -lt $runs_needed ]; do
     echo
@@ -37,5 +61,9 @@ while [ $runs_done -lt $runs_needed ]; do
     fi
     runs_done=$((++runs_done))
 done
+
+if [ "$cleanup_flag" == "-m" ]; then
+    mv_generated_files
+fi
 
 exit 0
